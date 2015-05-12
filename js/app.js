f769ba30ffe6f6c6015c893e7c3d7bcd69f5a305
rootmaker <speedtouchzh3@live.com.pt>
2015-05-12 13:26:09 +0000

    root

---
+++ js/app.js
@@ -0,0 +1,13 @@
+var app=angular.Module('app',[]);
+app.controller('testeCtlr', function($scope,$http,$window){
+    $scope.atividades=[];
+    $http.jsonp("https://gentle-caverns-6830.herokuapp.com/api/atividades/?format=jsonp&callback=JSON_CALLBACK")
+    .success(function(response){
+        $scope.atividades = response;
+        $window.localStorage.setItem('ativ',JSON.stringify(response));
+    })
+    .error(function(){
+        console.log("erro");
+        $scope.atividades = JSON.parse($window.localStorage.getItem('ativ'));
+    });
+});
